415b2efda7a287b274247f39410446d729c828fe
ADudin <gwaeron27@yandex.ru>
2021-02-05 19:35:32 +0300

    Completed module3-task1
    Добавил функцию для генерации случайных данных

--- js/main.js
+++ js/main.js
@@ -14,5 +14,71 @@ const getRandomFraction = function (min, max, fractionalSymbolsQuantity) {
   return 'Invalid range!';
 };
 
-getRandomInteger(12, 24);
-getRandomFraction(1.6, 1.7, 3);
+const TITLES = ['Тиёда', 'Синдзюку', 'Тосима', 'Бункё', 'Тайто', 'Кото', 'Минато', 'Аракава', 'Щибуя'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const CHECKOUTS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTIONS = ['Замечательное', 'Уютное', 'Изумительное', 'Восхитительное', 'Потрясающее', 'Современное'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const SIMILAR_ADVERTISEMENT_COUNT = 10;
+
+// for title, type, checkin, checkout, description keys:
+const getRandomArrayElement = function (array) {
+  return array[getRandomInteger(0, array.length - 1)];
+};
+
+// for photos key:
+const getRandomArray = function (array) {
+  const randomArrayLength = getRandomInteger(1, 10);
+  const newArray = [];
+  for (let i = 0; i <= randomArrayLength; i++) {
+    newArray.push(array[getRandomInteger(0, array.length - 1)]);
+  }
+  return newArray;
+};
+
+// for features key:
+const getRandomNoRepeatArray = function (array) {
+  const randomArrayLength = getRandomInteger(0, array.length - 1);
+  const newArray = [];
+  for (let i = 0; i <= randomArrayLength; i++) {
+    newArray.push(array.sort()[i]);
+  }
+  return newArray;
+};
+
+const getAdvertisement = function () {
+
+  const randomAuthor = {
+    avatar: 'img/avatars/user' + '0' + getRandomInteger(1, 8) + '.png',
+  };
+
+  const randomLocation = {
+    latitude: getRandomFraction(35.65, 35.7, 5),
+    longitude: getRandomFraction(139.7, 139.8, 5),
+  };
+
+  const randomOffer = {
+    title: 'Аренда в ' + getRandomArrayElement(TITLES),
+    address: randomLocation.latitude + ' северной широты, ' + randomLocation.longitude + ' восточной долготы',
+    price: getRandomInteger(2000, 10000),
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomInteger(1,5),
+    guests: getRandomInteger(1,10),
+    checkin: getRandomArrayElement(CHECKINS),
+    checkout: getRandomArrayElement(CHECKOUTS),
+    features: getRandomNoRepeatArray(FEATURES),
+    description: getRandomArrayElement(DESCRIPTIONS) + ' жильё!',
+    photos: getRandomArray(PHOTOS),
+  };
+
+  return {
+    author: randomAuthor,
+    offer: randomOffer,
+    location: randomLocation,
+  };
+};
+
+// eslint-disable-next-line
+const similarAdvertisements = new Array(SIMILAR_ADVERTISEMENT_COUNT).fill(null).map(() => getAdvertisement());
