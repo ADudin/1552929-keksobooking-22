d790a604eec41978503db2e3a3c9cd83973b5005
ADudin <gwaeron27@yandex.ru>
2021-03-06 17:44:40 +0300

    Complete module 6-task2
    Добавил валидацию формы.

--- js/form.js
+++ js/form.js
@@ -2,6 +2,9 @@ const type = document.querySelector('#type');
 const price = document.querySelector('#price');
 const timeIn = document.querySelector('#timein');
 const timeOut = document.querySelector('#timeout');
+const title = document.querySelector('#title');
+const roomNumber = document.querySelector('#room_number');
+const capacity = document.querySelector('#capacity');
 
 type.addEventListener('change', () => {
   switch (type.value) {
@@ -51,3 +54,35 @@ timeOut.addEventListener('change', () => {
       break;
   }
 });
+
+title.addEventListener('input', () => {
+  const valueLength = title.length;
+  if (valueLength < title.minlength) {
+    title.setCustomValidity('Ещё '+ (valueLength - title.minlength) + ' символов');
+  }
+  else if (valueLength > title.maxlength) {
+    title.setCustomValidity('Удалите лишние ' + (title.maxlength - valueLength) + ' символов');
+  }
+  else {
+    title.setCustomValidity('');
+  }
+  title.reportValidity();
+});
+
+const synchroniseRoomCapacity = () => {
+  if (Number(roomNumber.value) === 100 && Number(capacity.value) !== 0) {
+    capacity.setCustomValidity('Не верно указано количество гостей!');
+  }
+  else if (Number(capacity.value) === 0 && Number(roomNumber.value) !== 100) {
+    capacity.setCustomValidity('Не верно указано количество гостей!');
+  }
+  else if (Number(roomNumber.value) < Number(capacity.value)) {
+    capacity.setCustomValidity('Количество гостей не должно превышать количество комнат!');
+  } else {
+    capacity.setCustomValidity('');
+  }
+  capacity.reportValidity();
+};
+
+roomNumber.addEventListener('change', synchroniseRoomCapacity);
+capacity.addEventListener('change', synchroniseRoomCapacity);
